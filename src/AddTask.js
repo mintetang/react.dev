2b3c64ac08d8b7881885fb3911360d33ae953514
Joseph Tang <162398761+mintetang@users.noreply.github.com>
2026-02-01 11:45:05 +0000

    update 0201

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,18 +1,30 @@
-import { useState } from 'react';
+import { useState, useContext } from "react";
+import { useTasksDispatch } from "./TasksContext.js";
 
 export default function AddTask({ onAddTask }) {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
+  const dispatch = useTasksDispatch();
   return (
     <>
       <input
         placeholder="Add task"
         value={text}
-        onChange={e => setText(e.target.value)}
+        onChange={(e) => setText(e.target.value)}
       />
-      <button onClick={() => {
-        setText('');
-        onAddTask(text);
-      }}>Add</button>
+      <button
+        onClick={() => {
+          setText("");
+          dispatch({
+            type: "added",
+            id: nextId++,
+            text: text,
+          });
+        }}
+      >
+        Add
+      </button>
     </>
-  )
+  );
 }
+
+let nextId = 3;
