73bba9bc46f37c4ef85903069448f432acf3abcc
Joseph Tang <162398761+mintetang@users.noreply.github.com>
2026-02-03 11:14:03 +0000

    0203

---
+++ src/showhideinput.js
@@ -0,0 +1,74 @@
+<form id="form">
+  <label>
+    First name:
+    <b id="firstNameText">Jane</b>
+    <input
+      id="firstNameInput"
+      value="Jane"
+      style="display: none">
+  </label>
+  <label>
+    Last name:
+    <b id="lastNameText">Jacobs</b>
+    <input
+      id="lastNameInput"
+      value="Jacobs"
+      style="display: none">
+  </label>
+  <button type="submit" id="editButton">Edit Profile</button>
+  <p><i id="helloText">Hello, Jane Jacobs!</i></p>
+</form>
+
+<style>
+* { box-sizing: border-box; }
+body { font-family: sans-serif; margin: 20px; padding: 0; }
+label { display: block; margin-bottom: 20px; }
+</style>
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  if (editButton.textContent === "Edit Profile") {
+    editButton.textContent = "Save Profile";
+    hide(firstNameText);
+    hide(lastNameText);
+    show(firstNameInput);
+    show(lastNameInput);
+  } else {
+    editButton.textContent = "Edit Profile";
+    hide(firstNameInput);
+    hide(lastNameInput);
+    show(firstNameText);
+    show(lastNameText);
+  }
+}
+
+function handleFirstNameChange() {
+  firstNameText.textContent = firstNameInput.value;
+  helloText.textContent =
+    "Hello " + firstNameInput.value + " " + lastNameInput.value + "!";
+}
+
+function handleLastNameChange() {
+  lastNameText.textContent = lastNameInput.value;
+  helloText.textContent =
+    "Hello " + firstNameInput.value + " " + lastNameInput.value + "!";
+}
+
+function hide(el) {
+  el.style.display = "none";
+}
+
+function show(el) {
+  el.style.display = "";
+}
+
+let form = document.getElementById("form");
+let editButton = document.getElementById("editButton");
+let firstNameInput = document.getElementById("firstNameInput");
+let firstNameText = document.getElementById("firstNameText");
+let lastNameInput = document.getElementById("lastNameInput");
+let lastNameText = document.getElementById("lastNameText");
+let helloText = document.getElementById("helloText");
+form.onsubmit = handleFormSubmit;
+firstNameInput.oninput = handleFirstNameChange;
+lastNameInput.oninput = handleLastNameChange;
